76dd99a8ea115f6a697b3343a80a845748896354
Ryan.K <ryankung@ieee.org>
2017-11-19 20:25:04 +0800

    bugfixed

--- klefki/bitcoin/sign.py
+++ klefki/bitcoin/sign.py
@@ -1,18 +1,25 @@
 import base58
 from pyasn1.codec.der.encoder import encode
+from pyasn1.codec.der.decoder import decode
 from klefki.crypto.ecsda import sign, verify
-from klefki.asn import signature as sig
-from klefki.bitcoin.address import decode_privkey
+from klefki.asn import signature as sig_asn
+from klefki.bitcoin.address import decode_privkey, decode_pubkey
+from klefki.types.algebra.concrete import FiniteFieldCyclicBTC as FC
 
 
-def sign_with_privkey(privkey, msg):
-    res = sig.ECDSA_Sig_Value()
+def sign_with_privkey(privkey: str, msg: str):
+    struct = sig_asn.ECDSA_Sig_Value()
     key = decode_privkey(privkey)
-    res['r'], res['s'] = sign(key, msg)
-    return base58.b58encode(encode(res))
+    sig = sign(key, msg)
+    struct['r'], struct['s'] = sig[0].value, sig[1].value
+    return base58.b58encode(encode(struct))
 
 
-def verify_with_pubkey(pubkey, sig, msg):
-    sig, _ = base58.b58decode(sig, asn1Spec=sig.ECDSA_Sig_Value())
-    sig = sig[0]._value, sig[1]._value
+def verify_with_pubkey(pubkey: str, sig: str, msg: str):
+    pubkey = decode_pubkey(pubkey)
+    sig = base58.b58decode(sig)
+    sig, _ = decode(
+        sig, asn1Spec=sig_asn.ECDSA_Sig_Value()
+    )
+    sig = FC(sig[0]._value), FC(sig[1]._value)
     return verify(pubkey, sig, msg)
--- tests/test_sign.py
+++ tests/test_sign.py
@@ -1,9 +1,17 @@
 from klefki.crypto.ecsda import sign, verify, random_privkey, pubkey, proof
+from klefki.bitcoin.address import gen_key_pair
+from klefki.bitcoin.sign import sign_with_privkey, verify_with_pubkey
 
 
-def test_sigh():
+def test_sign():
     priv = random_privkey()
     pub = pubkey(priv)
     sig = sign(priv, 'test')
     assert verify(pub, sig, 'test')
     proof()
+
+
+def test_sign_btc():
+    privkey, pubkey = gen_key_pair()
+    sig = sign_with_privkey(privkey, 'test')
+    assert verify_with_pubkey(pubkey, sig, 'test')
