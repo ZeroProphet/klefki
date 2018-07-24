b14b0c5be07f621776393f43e3500be0bad2ebb4
Ryan.K <ryankung@ieee.org>
2018-07-24 17:59:26 +0800

    support bijection for eos public key

--- klefki/eos/public.py
+++ klefki/eos/public.py
@@ -5,16 +5,28 @@ from klefki.types.algebra.concrete import (
     FiniteFieldCyclicSecp256k1 as CF
 
 )
-from klefki.utils import ripemd160, b58encode
+from klefki.types.algebra.isomorphism import bijection, do
+from klefki.utils import ripemd160, b58encode, byte_to_int
 
 
 def to_bytes(pub: ECG) -> str:
     return bytes([2 + (pub.value[1].value % 2)]) + int_to_byte(pub.value[0].value)
 
+@bijection(to_bytes)
+def from_bytes(a):
+    return ECG([
+        CF(byte_to_int(a[:1]) - 2),
+        CF(byte_to_int(a[1:]))
+    ])
+
 
 def checksum(a) -> int:
     assert len(a) == 33
     return 'EOS' + b58encode(a + ripemd160(a)[:4])
+
+@bijection(checksum)
+def unchecksum(a):
+    return b58encode.inverse(a[3:])
 
 
 def gen_pub_key(key: CF):
