21aa4920634576898bc49871c2bd912ecea91e99
Ryan.K <ryankung@ieee.org>
2018-09-25 22:30:27 +0800

    privode default ishell

--- klefki/client/shell.py
+++ klefki/client/shell.py
@@ -1,5 +1,5 @@
-from functools import wraps
-
+from functools import wraps, partial
+import sys
 
 def command(fn, name=None):
 
@@ -8,6 +8,24 @@ def command(fn, name=None):
     fname = name or fn.__name__
     command.registered[fname] = fn
     return fn
+
+@command
+def shell():
+    '''
+    default Shell
+    '''
+    import random
+    from klefki.utils import to_sha256int
+    from klefki.types.algebra.concrete import (
+        JacobianGroupSecp256k1 as JG,
+        EllipticCurveCyclicSubgroupSecp256k1 as CG,
+        EllipticCurveGroupSecp256k1 as ECG,
+        FiniteFieldCyclicSecp256k1 as CF
+    )
+    import IPython
+    N = CG.N
+    G = CG.G
+    IPython.embed()
 
 
 @command
@@ -35,9 +53,19 @@ def router(cmds):
     'fn a b c=1 d=2'
     if isinstance(cmds, str):
         cmds = cmds.split(' ')
-    fn = command.registered.get(cmds[0], None)
+    try:
+        fn = command.registered.get(cmds[0], None)
+    except IndexError as _:
+        return help()
     if not fn:
         return not_found(cmds)
     args = [c for c in cmds[1:] if '=' not in c]
     kwargs = dict([c.split('=') for c in cmds[1:] if '=' in c])
     return fn(*args, **kwargs)
+
+
+main = partial(router, cmds=sys.argv[1:])
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -12,17 +12,31 @@ install_requires = [
         "r"
     )
 ]
-author = 'ryankung'
+author = 'Ryan Kung'
 email = 'ryankung@ieee.org'
 
 
 setup(
     name='klefki',
     description='Klefki is a playground for researching elliptic curve group based cryptocoins, such as Bitcoin and Ethereum. All data types & structures are based on mathematical defination of abstract algebra.',  # noqa
-    version='0.0.3.2',
+    url='https://github.com/RyanKung/klefki',
+    version='0.0.4.0',
     packages=find_packages(here, exclude=['tests']),
     license='GPL',
     author=author,
     author_email=email,
     install_requires=install_requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        "Programming Language :: Python :: 3.5",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    entry_points={
+        'console_scripts': [
+            'klefki=klefki.client.shell:main'
+        ]
+    }
 )
