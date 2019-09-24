3ec2f92fdd55eb38661c458e3fb99a943f9aa39a
Ryan.K <ryankung@ieee.org>
2019-09-24 21:49:50 +0800

    note

---
+++ klefki/types/algebra/utils.py
@@ -0,0 +1,18 @@
+from klefki.types.algebra.concrete import (
+    EllipticCurveGroupSecp256k1 as ECG,
+    FiniteFieldCyclicSecp256k1 as CF,
+    FiniteFieldSecp256k1 as F
+)
+from klefki.types.algebra.isomorphism import bijection
+
+def encode(key: ECG):
+    x = hex(key.value[0].value)[2:]
+    y = hex(key.value[1].value)[2:]
+    return '0' * (32 - len(x)) + x + '0' * (32 - len(y)) + y
+
+
+@bijection(encode)
+def decode(key: str) -> ECG:
+    x = F(int(key[:32], 16))
+    y = F(int(key[32:], 16))
+    return ECG((x, y))
