27cd412cfe3dad121767a0cb025a36b826400b0f
Ryan.K <ryankung@ieee.org>
2021-02-03 00:47:51 +0800

    refined flatten

--- klefki/zkp/r1cs.py
+++ klefki/zkp/r1cs.py
@@ -3,80 +3,68 @@ from https://github.com/ethereum/research/blob/master/zksnark/code_to_r1cs.py
 ref: https://medium.com/@VitalikButerin/quadratic-arithmetic-programs-from-zero-to-hero-f6d558cea649
 """
 
+from time import time
 import ast
 import inspect
-import types
 
-if 'arg' not in dir(ast):
-    ast.arg = type(None)
+class Flattener:
+    def __init__(self, src):
+        raw = ast.parse(src).body
+        assert len(raw) == 1 and isinstance(raw[0], ast.FunctionDef), "only support function"
+        self.raw = raw[0]
+        self.extra_inputs()
+        self.extra_body()
+        self._symbol = 0
+        self.flatten_body()
 
-def parse(code):
-    return ast.parse(code).body
+    def mk_symbol(self):
+        ret = "Sym-%s" % str(self._symbol)
+        self._symbol += 1
+        return ret
 
-def extract_inputs_and_body(code):
-    if len(code) != 1 or not isinstance(code[0], ast.FunctionDef):
-        raise Exception("Expecting function declaration")
-    # Gather the list of input variables
-    inputs = []
-    for arg in code[0].args.args:
-        if isinstance(arg, ast.arg):
-            assert isinstance(arg.arg, str)
-            inputs.append(arg.arg)
-        elif isinstance(arg, ast.Name):
-            inputs.append(arg.id)
-        else:
-            raise Exception("Invalid arg: %r" % ast.dump(arg))
-    # Gather the body
-    body = []
-    returned = False
-    for c in code[0].body:
-        if not isinstance(c, (ast.Assign, ast.Return)):
-            raise Exception("Expected variable assignment or return")
-        if returned:
-            raise Exception("Cannot do stuff after a return statement")
-        if isinstance(c, ast.Return):
-            returned = True
-        body.append(c)
-    return inputs, body
+    def extra_inputs(self):
+        self.inputs = [arg.arg for arg in self.raw.args.args]
 
-# Convert a body with potentially complex expressions into
-# simple expressions of the form x = y or x = y * z
-def flatten_body(body):
-    o = []
-    for c in body:
-        o.extend(flatten_stmt(c))
-    return o
+    def extra_body(self):
+        body = []
+        avalid_stmt = (ast.Assign, ast.Return)
+        returned = False
+        for c in self.raw.body:
+            assert isinstance(c, avalid_stmt)
+            assert not returned
+            if isinstance(c, ast.Return):
+                returned = True
+            body.append(c)
+        self.body = body
 
-# Generate a dummy variable
-next_symbol = [0]
-def mksymbol():
-    next_symbol[0] += 1
-    return 'sym_'+str(next_symbol[0])
-
-# "Flatten" a single statement into a list of simple statements.
-# First extract the target variable, then flatten the expression
-def flatten_stmt(stmt):
-    # Get target variable
-    if isinstance(stmt, ast.Assign):
-        assert len(stmt.targets) == 1 and isinstance(stmt.targets[0], ast.Name)
-        target = stmt.targets[0].id
-    elif isinstance(stmt, ast.Return):
-        target = '~out'
-    # Get inner content
-    return flatten_expr(target, stmt.value)
+    def flatten_body(self):
+        self.flatten_code = sum([self.flatten_stmt(c) for c in self.body], [])
 
 
-# Main method for flattening an expression
-def flatten_expr(target, expr):
-    # x = y
-    if isinstance(expr, ast.Name):
-        return [['set', target, expr.id]]
-    # x = 5
-    elif isinstance(expr, ast.Num):
-        return [['set', target, expr.n]]
-    # x = y (op) z
-    # Or, for that matter, x = y (op) 5
-    elif isinstance(expr, ast.BinOp):
+    def flatten_stmt(self, s):
+        if isinstance(s, ast.Assign):
+            assert len(s.targets) == 1 and isinstance(s.targets[0], ast.Name)
+            target = s.targets[0].id
+
+        elif isinstance(s, ast.Return):
+            target = '~out'
+        # Get inner content
+        return self.flatten_expr(target, s.value)
+
+    def flatten_expr(self, target, expr):
+        # x = y
+        if isinstance(expr, ast.Name):
+            return [['set', target, expr.id]]
+        # x = 5
+        elif isinstance(expr, ast.Num):
+            return [['set', target, expr.n]]
+
+        elif isinstance(expr, ast.BinOp):
+            return self.flatten_binop(target, expr)
+
+    def flatten_binop(self, target, expr):
+        avalid_binop = (ast.Mult, ast.Add, ast.Sub, ast.Div, ast.Pow)
+        assert type(expr.op) in avalid_binop
         if isinstance(expr.op, ast.Add):
             op = '+'
         elif isinstance(expr.op, ast.Mult):
@@ -85,49 +73,49 @@ def flatten_expr(target, expr):
             op = '-'
         elif isinstance(expr.op, ast.Div):
             op = '/'
-        # Exponentiation gets compiled to repeat multiplication,
-        # requires constant exponent
         elif isinstance(expr.op, ast.Pow):
-            assert isinstance(expr.right, ast.Num)
-            if expr.right.n == 0:
-                return [['set', target, 1]]
-            elif expr.right.n == 1:
-                return flatten_expr(target, expr.left)
-            else: # This could be made more efficient via square-and-multiply but oh well
-                if isinstance(expr.left, (ast.Name, ast.Num)):
-                    nxt = base = expr.left.id if isinstance(expr.left, ast.Name) else expr.left.n
-                    o = []
-                else:
-                    nxt = base = mksymbol()
-                    o = flatten_expr(base, expr.left)
-                for i in range(1, expr.right.n):
-                    latest = nxt
-                    nxt = target if i == expr.right.n - 1 else mksymbol()
-                    o.append(['*', nxt, latest, base])
-                return o
-        else:
-            raise Exception("Bad operation: " % ast.dump(expr.op))
-        # If the subexpression is a variable or a number, then include it directly
+            return self.flatten_pow(target, expr)
+
         if isinstance(expr.left, (ast.Name, ast.Num)):
             var1 = expr.left.id if isinstance(expr.left, ast.Name) else expr.left.n
             sub1 = []
         # If one of the subexpressions is itself a compound expression, recursively
         # apply this method to it using an intermediate variable
         else:
-            var1 = mksymbol()
-            sub1 = flatten_expr(var1, expr.left)
+            var1 = self.mk_symbol()
+            sub1 = self.flatten_expr(var1, expr.left)
         # Same for right subexpression as for left subexpression
         if isinstance(expr.right, (ast.Name, ast.Num)):
             var2 = expr.right.id if isinstance(expr.right, ast.Name) else expr.right.n
             sub2 = []
         else:
-            var2 = mksymbol()
-            sub2 = flatten_expr(var2, expr.right)
+            var2 = self.mk_symbol()
+            sub2 = self.flatten_expr(var2, expr.right)
         # Last expression represents the assignment; sub1 and sub2 represent the
         # processing for the subexpression if any
         return sub1 + sub2 + [[op, target, var1, var2]]
-    else:
-        raise Exception("Unexpected statement value: %r" % expr.value)
+
+
+    def flatten_pow(self, target, expr):
+        assert isinstance(expr.right, ast.Num)
+        if expr.right.n == 0:
+            return [['set', target, 1]]
+        elif expr.right.n == 1:
+            return self.flatten_expr(target, expr.left)
+        else: # This could be made more efficient via square-and-multiply but oh well
+            if isinstance(expr.left, (ast.Name, ast.Num)):
+                nxt = base = expr.left.id if isinstance(expr.left, ast.Name) else expr.left.n
+                o = []
+            else:
+                nxt = base = self.mk_symbol()
+                o = self.flatten_expr(base, expr.left)
+            for i in range(1, expr.right.n):
+                latest = nxt
+                nxt = target if i == expr.right.n - 1 else self.mk_symbol()
+                o.append(['*', nxt, latest, base])
+            return o
+
+
 
 # Adds a variable or number into one of the vectors; if it's a variable
 # then the slot associated with that variable is set to 1, and if it's
@@ -207,8 +195,9 @@ def assign_variables(inputs, input_vars, flatcode, field):
 
 
 def code_to_r1cs_with_inputs(code, input_vars, field):
-    inputs, body = extract_inputs_and_body(parse(code))
-    flatcode = flatten_body(body)
+    flatten = Flattener(code)
+    inputs = flatten.inputs
+    flatcode = flatten.flatten_code
     A, B, C = flatcode_to_r1cs(inputs, flatcode, field)
     r = assign_variables(inputs, input_vars, flatcode, field)
     return r, A, B, C
@@ -234,8 +223,9 @@ class R1CS:
     @staticmethod
     def r1cs(f, field=int):
         src = inspect.getsource(f)
-        inputs, body = extract_inputs_and_body(parse(src))
-        f.flatcode = flatten_body(body)
+        flatten = Flattener(src)
+        inputs = flatten.inputs
+        f.flatcode = flatten.flatten_code
         f.r1cs = flatcode_to_r1cs(inputs, f.flatcode, field)
         f.A = f.r1cs[0]
         f.B = f.r1cs[1]
