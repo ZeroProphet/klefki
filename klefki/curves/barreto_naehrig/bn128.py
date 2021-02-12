85eb0a615c18f2b92ac691f6160a23a65eee5754
Ryan.K <ryankung@ieee.org>
2021-02-13 00:42:43 +0800

    impl twist for bn128

--- klefki/curves/barreto_naehrig/bn128.py
+++ klefki/curves/barreto_naehrig/bn128.py
@@ -50,17 +50,36 @@ class ECGBN128(EllipticCurveGroup):
         return self.__class__((x, y))
 
     def twist(self):
-        # "Twist" a point in E(FQ2) into a point in E(FQ12)
         x, y = self.x, self.y
+        if isinstance(x, BN128FP12) and isinstance(y, BN128FP12):
+            return self
+        elif isinstance(x, BN128FP2) and isinstance(y, BN128FP2):
+            return self.twist_FP2_to_FP12(x, y)
+        elif isinstance(x, BN128FP) and isinstance(y, BN128FP):
+            return self.twist_FP_to_FP12(x, y)
+        else:
+            raise Exception("cannot twist curve to fp12")
+
+    @classmethod
+    def twist_FP_to_FP12(cls, x, y):
+        return cls(BN128FP12([x] + [BN128FP.zero()] * 11), BN128FP12([y] + [BN128FP.zero()] * 11))
+
+    @classmethod
+    def twist_FP2_to_FP12(cls, x, y):
+        # "Twist" a point in E(FQ2) into a point in E(FQ12)
         zero = BN128FP.zero()
         one = BN128FP.one()
         w = BN128FP12([zero, one] + [zero] * 10)
-
         assert isinstance(x, BN128FP2)
         assert isinstance(y, BN128FP2)
         nx = BN128FP12([x.id[0]] + [zero] * 5 + [x.id[1]] + [zero] * 5)
         ny = BN128FP12([y.id[0]] + [zero] * 5 + [y.id[1]] + [zero] * 5)
-        return self.type(nx / w **2, ny / w**3)
+        return cls((nx / w **2, ny / w**3))
+
+    def linefunc(self):
+        # https://github.com/ethereum/research/blob/9a7b6825b0dee7a59a03f8ca1d1ec3ae7fb6d598/zksnark/bn128_pairing.py
+        pass
+
 
 
 ECGBN128.G1 = ECGBN128(
