406cc0b933601c87ac29c99d7b19ffc12520e6c6
Ryan.K <ryankung@ieee.org>
2021-02-15 21:15:23 +0800

    fixed stupid syntax err

--- klefki/zkp/flatcode.py
+++ klefki/zkp/flatcode.py
@@ -69,9 +69,9 @@ class Flattener:
         assert loop.iter.func.id == "range"
         assert len(loop.iter.args) == 1
         assert isinstance(loop.iter.args[0], avalid_iter_arg), "%s is not support" % loop.iter.args[0]
-        if isinstance(loop.iter.args[0], (ast.Constant):
+        if isinstance(loop.iter.args[0], ast.Constant):
             times = loop.iter.args[0].value
-        elif isinstance(loop.iter.args[0], (ast.Num):
+        elif isinstance(loop.iter.args[0], ast.Num):
             times = loop.iter.args[0].n
         else:
             times = self.ctx[loop.iter.args[0].id]
