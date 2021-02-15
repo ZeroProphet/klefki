2992ed039aa3b1492f1ca8b9892e356019acc4b5
Ryan.K <ryankung@ieee.org>
2021-02-15 14:44:20 +0800

    set version to 1.5

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     name='klefki',
     description='Klefki is a playground for researching elliptic curve group based cryptocoins, such as Bitcoin and Ethereum. All data types & structures are based on mathematical defination of abstract algebra.',  # noqa
     url='https://github.com/RyanKung/klefki',
-    version='1.4.5',
+    version='1.5',
     packages=find_packages(here, exclude=['tests', "notes"]),
     license='GPL',
     author=author,
