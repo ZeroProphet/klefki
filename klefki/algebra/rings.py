63ca1c682854a0147a799cd476f36ed108a101d3
Ryan.K <ryankung@ieee.org>
2021-02-17 19:53:58 +0800

    impl with fmap

--- klefki/algebra/rings.py
+++ klefki/algebra/rings.py
@@ -3,36 +3,84 @@ from operator import add, mul
 from operator import neg
 from itertools import starmap
 
+__all__ = ["PolyRing"]
+
+def _multiply_polys(a, b):
+    o = [0] * (len(a) + len(b) - 1)
+    for i in range(len(a)):
+        for j in range(len(b)):
+            o[i + j] += a[i] * b[j]
+    return o
+
+
+def _add_polys(a, b):
+    o = [0] * max(len(a), len(b))
+    for i in range(len(a)):
+        o[i] += a[i]
+    for i in range(len(b)):
+        o[i] += b[i]
+    return o
+
+def _neg_poly(a):
+    return [-x for x in a]
+
+
+def _div_polys(a, b):
+    o = [0] * (len(a) - len(b) + 1)
+    remainder = a
+    while len(remainder) >= len(b):
+        leading_fac = remainder[-1] / b[-1]
+        pos = len(remainder) - len(b)
+        o[pos] = leading_fac
+        remainder = _add_polys(
+            remainder,
+            _neg_poly(
+                _multiply_polys(
+                    b,
+                    [0] * pos + [leading_fac]
+                )
+            )
+        )[:-1]
+    return o
+
+
 
 class PolyRing(Ring):
-
     @property
     def degree(self):
         return len(self.id)
 
     def op(self, rhs: Ring):
-        return self.type(list(starmap(add, zip(self.id, rhs.id))))
+        return self.fmap(_add_polys)(self, rhs)
 
     def inverse(self):
-        return self.type(list(map(neg, self.id)))
+        return self.fmap(_neg_poly)(self)
 
     def sec_op(self, rhs: Ring):
-        return self.type(list(starmap(mul, zip(self.id, rhs.id))))
+        return self.fmap(_multiply_polys)(self, rhs)
+
+    def div(self, rhs: Ring):
+        return self.fmap(_div_polys)(self, rhs)
 
     def __truediv__(self, rhs: Ring):
-        a = self.id
-        b = rhs.id
-        cls = self.__class__
-        o = [0] * (len(a) - len(b) + 1)
-        remainder = a
-        f = self.lift_fmap(lambda x, y, z: x - (y * z))
-        while len(remainder) >= len(b):
-            leading_fac = remainder[-1] // b[-1]
-            pos = len(remainder) - len(b)
-            o[pos] = leading_fac
-            remainder = f(
-                remainder,
-                b,
-                [0] * pos + [leading_fac]
-            ).id
-            return cls(o)
+        return self.div(rhs)
+
+
+    @classmethod
+    def singleton(cls, point_loc, height, total_pts):
+        fac = 1
+        for i in range(1, total_pts + 1):
+            if i != point_loc:
+                fac *= point_loc - i
+        o = cls([height // fac])
+        for i in range(1, total_pts + 1):
+            if i != point_loc:
+                o = cls.lift_fmap(mul)(o.id, [-i, 1])
+        return o
+
+    @classmethod
+    def lagrange_interp(cls, vec):
+        o = cls([])
+        for i in range(len(vec)):
+            o = o + cls.singleton(i + 1, vec[i], len(vec))
+        return o
--- tests/algebras/test_rings.py
+++ tests/algebras/test_rings.py
@@ -6,3 +6,9 @@ def test_poly_neg():
     r = PolyRing(poly)
     assert (-r).id == [-1, -2, -3, -4, -5]
     assert (r / r).id == [1]
+
+
+def test_lagrange_poly():
+    vec = [1, 2, 3, 4]
+    ret = PolyRing.lagrange_interp(vec)
+    assert ret.id == [28, -35, 15, -2]
