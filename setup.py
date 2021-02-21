9d8036a11f26f7bd5290b1a9ea44628d1201ea49
Ryan.K <ryankung@ieee.org>
2021-02-21 19:51:09 +0800

    pump to 1.6.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/RyanKung/klefki',
-    version='1.5.6',
+    version='1.6.1',
     packages=find_packages(here, exclude=['tests', "notes"]),
     license='GPL',
     author=author,
